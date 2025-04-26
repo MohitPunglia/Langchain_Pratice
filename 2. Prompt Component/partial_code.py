57647f7c291a9484bdb6e68e24519f28aaaa5d3a
Mohit <mohit.punglia@gmail.com>
2025-04-26 08:44:15 +0530

    Added Partial code

---
+++ 2. Prompt Component/partial_code.py
@@ -0,0 +1,27 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id="Qwen/Qwen1.5-0.5B-Chat", task="text2text-generation")
+
+model = ChatHuggingFace(llm=llm)
+
+st.header("Hugging Face Chat Model")
+
+paper_input = st.selectbox(
+    "Select Research Paper Name",
+    [
+        "Attention Is All You Need",
+        "BERT: Pre-training of Deep Bidirectional Transformers",
+        "GPT-3: Language Models are Few-Shot Learners",
+        "Diffusion Models Beat GANs on Image Synthesis",
+    ],
+)
+
+style_input = st.selectbox(
+    "Select Explanation Style",
+    ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"],
+)
