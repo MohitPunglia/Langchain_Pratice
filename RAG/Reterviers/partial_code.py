2edef58a362e3febe20486b7f18ca0e53d777545
Mohit <mohit.punglia@gmail.com>
2025-06-21 13:33:17 +0530

    Added partial code

--- RAG/Reterviers/partial_code.py
+++ RAG/Reterviers/partial_code.py
@@ -29,3 +29,37 @@ documents = [
     Document(page_content="Embeddings convert text into high-dimensional vectors."),
     Document(page_content="OpenAI provides powerful embedding models."),
 ]
+
+# %%
+# Initialize open-source embeddings
+embedding_function=HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+# %%
+vector_store=Chroma.from_documents(
+    documents=documents,
+    embedding=embedding_function,
+    collection_name='my_collection'
+)
+
+
+# %%
+retriever=vector_store.as_retriever()
+
+# %%
+query='what is Chroma used for?'
+results=retriever.invoke(query)
+
+for i in results:
+  print('Result ')
+
+  print(i.page_content)
+
+
+
+# %%
+vector_store.similarity_search(query,k=2)
+
+# %% [markdown]
+# # MMR Maximum Marginal Relevance Code Example
