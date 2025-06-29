1016e1737b777551b66d504147f00147031ff022
Mohit <mohit.punglia@gmail.com>
2025-06-29 22:35:16 +0530

    Added partial code

---
+++ RAG/Vector Store/partial_code.py
@@ -0,0 +1,16 @@
+ %%
+!pip install langchain chromadb pypdf langchain-community
+
+# %%
+from langchain.vectorstores import Chroma
+from langchain.schema import Document
+
+# %%
+doc1 = Document(
+        page_content="Virat Kohli is one of the most successful and consistent batsmen in IPL history. Known for his aggressive batting style and fitness, he has led the Royal Challengers Bangalore in multiple seasons.",
+        metadata={"team": "Royal Challengers Bangalore"}
+    )
+doc2 = Document(
+        page_content="Rohit Sharma is the most successful captain in IPL history, leading Mumbai Indians to five titles. He's known for his calm demeanor and ability to play big innings under pressure.",
+        metadata={"team": "Mumbai Indians"}
+    )
